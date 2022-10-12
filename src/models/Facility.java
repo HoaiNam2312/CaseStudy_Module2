62ce95174a56949ec3c5cd40ebc1006fb05329ed
HoaiNam2312 <namhoai2312@gmail.com>
2022-10-12 17:56:09 +0700

    first commit

---
+++ src/models/Facility.java
@@ -0,0 +1,11 @@
+package services;
+
+public abstract class Facility {
+    private String tenDichVu;
+    private double dienTichSuDung;
+    private int chiPhiThue;
+    private int soLuongNguoiToiDa;
+    private String kieuThue;
+
+
+}
